b74c1ec46a039dc241915227071578353e75ffd0
Miguel <migueldiniz25@gmail.com>
2017-02-03 11:22:22 +0100

    added check permission

--- app/src/main/java/com/example/mymap/MapsActivity.java
+++ app/src/main/java/com/example/mymap/MapsActivity.java
@@ -57,6 +57,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == LOCATION_REQUEST_CODE) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
             mMap.setMyLocationEnabled(true);
         }
     }
