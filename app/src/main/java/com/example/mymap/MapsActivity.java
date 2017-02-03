300d7a7bb563e27e0c9de9e8cf8acff9ab829056
Miguel <migueldiniz25@gmail.com>
2017-02-03 12:32:55 +0100

    peticion de mi localizacion

--- app/src/main/java/com/example/mymap/MapsActivity.java
+++ app/src/main/java/com/example/mymap/MapsActivity.java
@@ -1,12 +1,14 @@
 package com.example.mymap;
 
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -23,8 +25,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         GoogleApiClient.OnConnectionFailedListener{
 
     private static final int LOCATION_REQUEST_CODE = 1; //peticion de la ubicacion
+    private static final int REQUEST_LOCATION = 2; //obtencion de latitud y longitud
     private GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
+    private Location myLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +66,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.getUiSettings().setMapToolbarEnabled(false);
 
         LatLng treasure_latlng = new LatLng(42.236905, -8.712710);
-        Marker treasure = mMap.addMarker(new MarkerOptions().position(treasure_latlng).title("tesoro"));
+        mMap.addMarker(new MarkerOptions().position(treasure_latlng).title("tesoro"));
 
     }
 
@@ -77,11 +81,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
             mMap.setMyLocationEnabled(true);
         }
+        if (requestCode == REQUEST_LOCATION) {
+            if (grantResults.length == 1
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                myLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                if (myLocation != null) {
+
+
+                } else {
+                    Toast.makeText(this, "Ubicación no encontrada", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Toast.makeText(this, "Permisos no otorgados", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Aquí muestras confirmación explicativa al usuario
+                // por si rechazó los permisos anteriormente
+            } else {
+                ActivityCompat.requestPermissions(
+                        this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_LOCATION);
+            }
+        } else {
+            myLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (myLocation != null) {
+
+
+
+            } else {
+                Toast.makeText(this, "Ubicación no encontrada", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
