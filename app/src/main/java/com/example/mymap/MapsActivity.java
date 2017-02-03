0d95bf354e16ad2f6653dff90e13f5f26a718e6c
Miguel <migueldiniz25@gmail.com>
2017-02-03 11:34:28 +0100

    changed map

--- app/src/main/java/com/example/mymap/MapsActivity.java
+++ app/src/main/java/com/example/mymap/MapsActivity.java
@@ -15,7 +15,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private static final int LOCATION_REQUEST_CODE = 1;
+    private static final int LOCATION_REQUEST_CODE = 1; //peticion de la ubicacion
     private GoogleMap mMap;
 
     @Override
@@ -28,16 +28,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -52,6 +42,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
             }
         }
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+
     }
 
     @Override
