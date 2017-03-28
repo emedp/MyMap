db3e047bfb177c4d2d7af110e7c8059eddac5d56
Mikys <migueldiniz25@gmail.com>
2017-03-28 20:40:53 +0200

    implementado objeto

--- app/src/main/java/com/example/mymap/ScannerActivity.java
+++ app/src/main/java/com/example/mymap/ScannerActivity.java
@@ -1,8 +1,20 @@
 package com.example.mymap;
 
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
 /**
  * Created by Mikys on 28/03/2017.
  */
 
-public class ScannerActivity {
+public class ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+
+    @Override
+    public void handleResult(Result result) {
+        
+    }
 }
